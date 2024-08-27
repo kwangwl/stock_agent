3bddc1146076c7d1686568d7263eb2fa9445c76f
kwolell <kwolell@gmailc.om>
2024-08-27 15:30:43 +0900

    init commit

---
+++ agent_app.py
@@ -0,0 +1,93 @@
+import streamlit as st
+import agent_lib as glib
+import pandas as pd
+import uuid
+import json
+
+
+# config
+AGENT_ID = ""
+AGENT_ALIAS_ID = ""
+
+
+# function
+def display_stock_chart(trace):
+    chart_text = trace.get('observation', {}).get('actionGroupInvocationOutput', {}).get('text')
+    chart = json.loads(chart_text)
+
+    df = pd.DataFrame(list(chart.items()), columns=['Date', 'Closing Price'])
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+
+    st.markdown("**주식 차트**")
+    st.line_chart(df, x_label="날짜", y_label="종가")
+
+
+def display_stock_balance(trace):
+    balance_text = trace.get('observation', {}).get('actionGroupInvocationOutput', {}).get('text')
+    balance = json.loads(balance_text)
+
+    df = pd.DataFrame.from_dict(balance, orient='index').transpose()
+
+    st.markdown("**재무 제표**")
+    st.dataframe(df, use_container_width=True)
+
+
+def display_recommendations(trace):
+    recommendations_text = trace.get('observation', {}).get('actionGroupInvocationOutput', {}).get('text')
+    recommendations = json.loads(recommendations_text)
+
+    df = pd.DataFrame.from_dict(recommendations, orient='index').transpose()
+
+    st.markdown("**애널리스트 추천**")
+    st.dataframe(df, use_container_width=True)
+
+
+# main page
+st.set_page_config(page_title="주식 분석 에이전트")
+st.title("주식 분석 에이전트")
+
+# session 초기화
+if 'session_stock_agent' not in st.session_state:
+    st.session_state['session_stock_agent'] = ""
+
+input_text = st.text_area("종목명을 입력하세요  (한글 이름 or 영어 이름 or 야후 파이낸스 ticker 입력 가능)",
+                          value=st.session_state['session_stock_agent'])
+submit_button = st.button("분석 시작", type="primary")
+
+if submit_button and input_text:
+    # session 저장
+    st.session_state['session_stock_agent'] = input_text
+
+    with st.spinner("응답 생성 중..."):
+        # 에이전트 호출 (세션은 항상 초기화 하도록 구성)
+        response = glib.get_agent_response(
+            AGENT_ID,
+            AGENT_ALIAS_ID,
+            str(uuid.uuid4()),
+            input_text
+        )
+
+        # 응답 출력
+        output_text = response["output_text"]
+        st.subheader("분석 결과")
+        st.write(output_text)
+
+        # 추적 정보 표시
+        st.divider()
+        st.subheader("추론 기록")
+
+        if response["trace"] and "orchestrationTrace" in response["trace"]:
+            traces = response["trace"]["orchestrationTrace"]
+            for index, item in enumerate(traces):
+                function_name = item.get('invocationInput', {}).get('actionGroupInvocationInput', {}).get(
+                    'function', "")
+
+                if function_name == "get_stock_chart":
+                    display_stock_chart(traces[index + 1])
+
+                elif function_name == "get_stock_balance":
+                    display_stock_balance(traces[index + 1])
+
+                elif function_name == "get_recommendations":
+                    display_recommendations(traces[index + 1])
---
+++ agent_lib.py
@@ -0,0 +1,42 @@
+import boto3
+
+
+def get_agent_response(agent_id, agent_alias_id, session_id, prompt):
+    """Get a response from the Bedrock agent using specified parameters."""
+
+    # Create a Boto3 client for the Bedrock Runtime service
+    session = boto3.Session()
+    bedrock_agent = session.client(service_name='bedrock-agent-runtime', region_name='us-west-2')
+
+    # Invoke the Bedrock agent with the specified parameters
+    response = bedrock_agent.invoke_agent(
+        agentId=agent_id,
+        agentAliasId=agent_alias_id,
+        enableTrace=True,
+        sessionId=session_id,
+        inputText=prompt,
+    )
+
+    # Initialize variables to store the output, citations, and trace information
+    output_text = ""
+    trace = {}
+
+    # Process each event in the response
+    for event in response.get("completion"):
+        # Combine the chunks to get the output text
+        if "chunk" in event:
+            chunk = event["chunk"]
+            output_text += chunk["bytes"].decode()
+
+        # Extract trace information from all events
+        if "trace" in event:
+            if "orchestrationTrace" in event["trace"]["trace"]:
+                if "orchestrationTrace" not in trace:
+                    trace["orchestrationTrace"] = []
+                trace["orchestrationTrace"].append(event["trace"]["trace"]["orchestrationTrace"])
+
+    # Return the processed output, citations, and trace information
+    return {
+        "output_text": output_text,
+        "trace": trace
+    }
